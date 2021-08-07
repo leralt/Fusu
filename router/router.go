3b299676c4c1048718873443e09e22ecf7aabf2d
leralt <htliucl@foxmail.com>
2021-08-07 20:42:37 +0800

    gin first

--- router/router.go
+++ router/router.go
@@ -1,57 +1,23 @@
 package router
 
 import (
-	"Fusu/ws"
-	"fmt"
-	"io/ioutil"
-	"log"
-	"net/http"
-	"os"
+	"github.com/gin-gonic/gin"
 )
 
 
 
 func Start() {
+	r := gin.Default()
+	r.Static("/static","./static")
+	r.StaticFile("/", "./views/index.html")
 	//fmt.Println("init...")
-	http.HandleFunc("/static/",doExecute)
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "views/index.html")
-	})
-	http.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
-		if r.FormValue("username") == "long" && r.FormValue("password") == "123" {
-			http.ServeFile(w,r,"views/room.html")
-		}
-	})
-	http.HandleFunc("/room", ws.EchoMessage)
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		fmt.Println("Listen and serve error",err)
-		return
-	}
-}
+	r.StaticFile("/room", "./views/room.html")
 
-var realPath ="/Users/long/Projects/Fusu"
-func doExecute( response http.ResponseWriter,request *http.Request) {
-	requestUrl :=request.URL.String()
-	fmt.Println(requestUrl[:])
-	filePath := requestUrl[len("/static"):]
-	fmt.Println("requestUrl =",filePath)
-	file,err :=os.Open(realPath + requestUrl)
-	defer func(file *os.File) {
-		err := file.Close()
-		if err != nil {
-			fmt.Println("file Close error",err)
-		}
-	}(file)
-	if err != nil {
-		log.Println("static resource:", err)
-		response.WriteHeader(404)
-	} else {
-		bs,_ := ioutil.ReadAll(file)
+	r.POST("/login", func(context *gin.Context) {
 
-		_, err := response.Write(bs)
-		if err != nil {
-			return
-		}
+	})
+	err := r.Run()
+	if err != nil {
+		return 
 	}
 }
