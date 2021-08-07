0e05e4750e68619f4bd578620817880de61f0916
leralt <htliucl@foxmail.com>
2021-08-07 21:26:59 +0800

    gin second

--- login/login.go
+++ login/login.go
@@ -1,1 +1,22 @@
 package login
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type req struct {
+	Name string `json:"username"`
+	Pass  string `json:"password"`
+}
+func Auth(c *gin.Context) {
+	form := req{}
+	if err := c.BindJSON(&form); err != nil {
+		c.AbortWithStatus(http.StatusBadRequest)
+		return
+	}
+
+	if form.Name == "long" && form.Pass == "123"{
+
+	}
+}
--- router/router.go
+++ router/router.go
@@ -2,6 +2,7 @@ package router
 
 import (
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 
@@ -12,9 +13,13 @@ func Start() {
 	r.StaticFile("/", "./views/index.html")
 	//fmt.Println("init...")
 	r.StaticFile("/room", "./views/room.html")
-
+	r.StaticFile("/register","./views/register.html")
 	r.POST("/login", func(context *gin.Context) {
-
+		username := context.PostForm("username")
+		password := context.PostForm("password")
+		if username == "long" && password == "123" {
+			context.Redirect(http.StatusMovedPermanently, "http://localhost:8080/room")
+		}
 	})
 	err := r.Run()
 	if err != nil {
